657ba3313a0cc08b9814e9def0600457f623c084
Jacob Ferrier <jferrier@uncc.edu>
2019-03-19 18:53:42 -0400

    Continued working on store catalogs

--- databases/library/modules/createStoreCatalog.py
+++ databases/library/modules/createStoreCatalog.py
@@ -10,18 +10,20 @@ def __main__(wd, numIngredientsPerStore, storeNum, availableIngredientList, stor
 		chunk.write('#store' + str(storeNum) + '\t' + '(' + str(random.randint(0,201)-100) + ',' + str(random.randint(0,201)-100) + ')\n')
 		chunk.write('#ingredient_name\tingredient_price\tingredient_tag\n')
 		
-		parseAvailableIngredients(wd, availableIngredientList)
-
+		ingredientDictionary = parseAvailableIngredients(wd, availableIngredientList)
+		print(ingredientDictionary)
 		#for i in range(numIngredientsPerStore): chunk.write()
 	chunk.close()
 
 def parseAvailableIngredients(wd, input):
 	os.chdir(wd + '/library/data')
 	
-	ingredientDictionary = {}
+	dict = {}
 	with open(input, 'r') as file:
 		for line in file:
 			if line.startswith('#'): pass
-			else: print(line.strip().split('\t'))
-			else: ingredientDictionary.add
+			#else: print(line.strip().split('\t'))
+			else: dict[line.strip().split('\t')[0]] = [line.strip().split('\t')[1], line.strip().split('\t')[2]]
 	file.close()
+	
+	return(dict)

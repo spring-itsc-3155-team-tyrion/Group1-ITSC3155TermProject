d0ecd5c0d33f60622f9ed3764c602cade4915ae0
Jacob Ferrier <jferrier@uncc.edu>
2019-03-19 17:22:46 -0400

    Added module to generate available ingredient list

---
+++ databases/library/modules/createAvailableIngredientList.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import random
+
+def __main__(wd, numIngredients, ingredientsListFile):
+	os.chdir(wd + '/library/data')
+
+	with open(ingredientsListFile, 'w') as chunk:
+		chunk.write('ingredient_name\tingredient_price\tingredient_special_tag\n')
+		for i in range(numIngredients): chunk.write('ingredient_' + ('%0' + str(len(str(numIngredients))) + 'd') % i + '\t'
+			+ str(float(random.randint(1, 1001))/100.00) + '\t' + 'null' + '\n')
+	chunk.close()
--- databases/main.py
+++ databases/main.py
@@ -5,14 +5,24 @@ import os
 ### parameters
 mainDir = os.getcwd()
 numStores = 4
-numIngredients = (numStores + 1)*100
-ingredientsList = 'ingredientsList.list'
+numIngredientsPerStore = 100
+numIngredientsTotal = numIngredientsPerStore + numStores*20 #20 extra ingredients per store
+availableIngredientsList = 'availableIngredientList.list'
 
 try:
-	print('\n------Creating ingredient list-----\n')
-	from library.modules import createIngredientList
-	createIngredientList.__main(mainDir, numIngredients, ingredientsList)
+	print('\n-----Creating availble ingredient list-----\n')
+	from library.modules import createAvailableIngredientList
+	createAvailableIngredientList.__main__(mainDir, numIngredientsTotal, availableIngredientsList)
 	print('Done')
 except Exception as e:
-	print('Exception found during creating of ingredient list: ' + str(e))
+	print('Exception found during creating ingredient list: ' + str(e))
+	exit()
+
+try:
+	print('\n-----Creating store catalogs----\n')
+	from library.modules import createStoreCatalog
+	for i in range(numStores): createStoreCatalog.__main__(mainDir, numIngredientsPerStore, i, availableIngredientsList)
+	print('Done')
+except Exception as e:
+	print('Exception found during creating store catalogs: ' + str(e))
 	exit()

ee22360908e43ba2f7dc8a62654ca4027308908d
Jacob Ferrier <jferrier@uncc.edu>
2019-03-25 16:46:25 -0400

    Started working on creating a unified price matrix

---
+++ algorithms/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+
+### parameters
+mainDir = os.getcwd()
+
+try:
+	print('\n-----Creating price matrix -----\n')
+	from library.modules import createPriceMatrix
+	createPriceMatrix.__main__(mainDir, ingredientList)
+	print('Done')
+except Exception as e:
+	print('Exception found during creating price matrix: ' + str(e))
+	exit()

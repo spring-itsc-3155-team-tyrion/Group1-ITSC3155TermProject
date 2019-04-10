ef4fff66ce9251726a630eac630a627caa58929f
stayl116 <stayl116@uncc.edu>
2019-04-10 14:43:52 +0000

    Basket Controller setup, new routes using basket controller, set up loop on view for when activerecord is done

--- frontEnd/new/BudgetBasket/app/controllers/baskets_controller.rb
+++ frontEnd/new/BudgetBasket/app/controllers/baskets_controller.rb
@@ -1,2 +1,23 @@
 class BasketsController < ApplicationController
+    
+    def basket
+       # @baskets = Basket.all
+    end
+    
+    def calculation
+    end
+    
+    def create
+        @basket = Basket.new(basket_params)
+        
+        @basket.save
+        redirect_to @basket
+    end
+    
+    def destroy
+        @basket = Basket.find(params[:id])
+        @basket.destroy
+        
+        redirect_to recipes_path
+    end
 end
--- frontEnd/new/BudgetBasket/config/routes.rb
+++ frontEnd/new/BudgetBasket/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get '/allRecipes' => 'recipes#allRecipes'
-    get '/basket' => 'recipes#basket'
-    get '/calculation' => 'recipes#calculation'
+    get '/basket' => 'baskets#basket'
+    get '/calculation' => 'baskets#calculation'
     root 'recipes#home'
 end

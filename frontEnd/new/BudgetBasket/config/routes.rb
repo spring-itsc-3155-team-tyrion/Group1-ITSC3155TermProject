eb25f864aa973e8defd616238f2cc1b379e686be
Jacob Ferrier <jferrier@uncc.edu>
2019-04-13 16:43:58 +0000

    fixed route for basket#allBaskets

--- frontEnd/new/BudgetBasket/config/routes.rb
+++ frontEnd/new/BudgetBasket/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get '/allRecipes' => 'recipes#allRecipes'
-    get '/basket' => 'baskets#basket'
+    get '/basket' => 'baskets#allBaskets'
     get '/calculation' => 'baskets#calculation'
     get 'button' => 'start_up#button'
     get 'addRecipeToBasketButton' => 'recipes#addRecipeToBasket'

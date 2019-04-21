75f1e46f02fd8673a6f07337ea39218a6ec20b50
Jacob Ferrier <jferrier@uncc.edu>
2019-04-21 20:51:59 +0000

    Added delete from basket functionality to recipes_controller

--- frontEnd/BudgetBasket/app/controllers/recipes_controller.rb
+++ frontEnd/BudgetBasket/app/controllers/recipes_controller.rb
@@ -45,7 +45,37 @@ class RecipesController < ApplicationController
     
     def deleteRecipeFromBasket
         @recipe = Recipe.find(params[:recipe])
-        system ("echo 'delete recipe '" + @recipe.recipe_name)
+        
+        countArray = []
+        #fill countArray which tracks number of occurences of ingredients in basket (varifies that the recipe was actually added to the basket)
+        for ingredient in @recipe.recipe_ingredient_array do
+            if Basket.exists?(ingredient_name: ingredient)
+                @basket = Basket.find_by(ingredient_name: ingredient)
+                countArray[countArray.length] = @basket.ingredient_count
+            else
+                countArray[countArray.length] = 0
+            end
+        end
+        
+        system ("echo " + countArray.to_s)
+        system ("echo " + (countArray.min).to_s)
+        
+        if countArray.min != 0
+            for ingredient in @recipe.recipe_ingredient_array do
+                if Basket.exists?(ingredient_name: ingredient)
+                    @basket = Basket.find_by(ingredient_name: ingredient)
+                    @basket.ingredient_count = @basket.ingredient_count - 1
+                    @basket.save
+                    if @basket.ingredient_count == 0
+                        @basket.destroy
+                    end
+                else
+                    system ("echo " + "critical erorr in deleteRecipeFromBasket method")
+                end
+            end
+        else
+            system ("echo " + "user tried deleting a recipe that was never added to the basket")
+        end
         
         redirect_to allRecipes_path
     end
